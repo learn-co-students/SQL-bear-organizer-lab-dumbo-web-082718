bce070474bcf7726ae6686fad3e3cf502b172887
jkhaha <jha2@wellesley.edu>
2018-09-04 15:28:35 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 2, 'male', 'brown', 'nice', 0);
+
+INSERT INTO bears VALUES (2, 'Rowdy', 2, 'male', 'black', 'nice', 1);
+
+INSERT INTO bears VALUES (3, 'Tabitha', 9, 'female', 'blue', 'nice', 0);
+
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 9, 'male', 'red', 'mean', 1);
+
+INSERT INTO bears VALUES (5, 'Melissa', 4, 'female', 'brown', 'nice', 0);
+
+INSERT INTO bears VALUES (6, 'Grinch', 14, 'male', 'black', 'nice', 1);
+
+INSERT INTO bears VALUES (7, 'Wendy', 9, 'female', 'blue', 'mean', 0);
+
+INSERT INTO bears VALUES (8, null, 4, 'female', 'brown', 'nice', 1);
